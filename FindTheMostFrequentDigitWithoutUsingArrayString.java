6d470ecc7dedcad2195c91d7eb82f363b23c9e7a
jatin goswami <47290228+jatingoswami1988@users.noreply.github.com>
2022-07-25 13:37:59 +0530

    Add files via upload

---
+++ FindTheMostFrequentDigitWithoutUsingArrayString.java
@@ -0,0 +1,18 @@
+package com.fis.Strings.OccurenceBased;
+
+public class FindTheMostFrequentDigitWithoutUsingArrayString {
+
+	public static void main(String[] args) {
+		
+		
+		int no = 1223344555;
+		int result = countFrequency(no);
+
+	}
+
+	private static int countFrequency(int no) {
+		
+		return 0;
+	}
+
+}
